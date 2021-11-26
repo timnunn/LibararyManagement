c9f5d2ce4b01916dd42b45a4868b482965946adc
Timothy Nunn <54078499+timnunn@users.noreply.github.com>
2021-11-25 19:25:20 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMS1.Startup))]
+namespace LMS1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
